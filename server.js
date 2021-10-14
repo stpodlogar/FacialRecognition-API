54196142291dc28c56970843d09688f990e453b4
Stephen Podlogar <stephenpodlogar@Stephens-MacBook-Pro.local>
2021-10-14 17:39:21 -0400

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-opaque-25081',
     user : 'stephenpodlogar',
     password : '',
     database : 'smart-brain'
