25f55d5c1f4e5f115d77783605a13724547c6017
Stephen Podlogar <stephenpodlogar@Stephens-MacBook-Pro.local>
2021-10-14 17:41:55 -0400

    adding db connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : 'postgresql-opaque-25081',
-    user : 'stephenpodlogar',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl : true
   }
 });
 
